0281ecf416d866711dee154e137ad5c627921e4b
colconstruccion <rvelezpantoja@gmail.com>
2025-07-09 14:48:06 -0400

    Update app.js
    Se removio codigo que no tenia que crear la aplicacion de convertir millas a kilometros

--- app.js
+++ app.js
@@ -6,20 +6,3 @@ let kiloValue = numMiles * 1.60934;
 console.log(kiloValue);
 let message = `${numMiles} miles is equal to ${kiloValue} kilometers`;
 console.log(message);
-
-//lesson 19
-/*let checkNum = 5;
-if (checkNum > 10){
-    console.log("Yes, greater than 10");
-}
-else if (checkNum == 50){
-    console.log("It's equal to 50");
-}
-else if(checkNum === 5){
-    console.log("Yes, it's equal to 5");
-}else{
-    console.log("the number is "+checkNum);
-}
-
-console.log(checkNum);
-*/
