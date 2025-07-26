18e99505fdd9614393b6485d2a0c875ecaa73ca2
colconstruccion <rvelezpantoja@gmail.com>
2025-07-25 23:51:28 -0400

    Update converter.js
    funcionalidad para otras conversiones

--- converter.js
+++ converter.js
@@ -1,14 +1,24 @@
-convert =  document.querySelectorAll("li a");
+const convert =  document.querySelectorAll("li a");
 h1 = document.querySelector("h1");
 for (x=0;x<convert.length;x++){
-    //let unitC = convert[x].id;
+    // cambiar la clase del li
+    convert[x].addEventListener("mouseover",function(){
+        this.style.color = "blue";
+        this.style.backgroundColor = "yellow";
+        this.style.cursor = "pointer";
+    })
+
+    convert[x].addEventListener("mouseout",function(){
+        this.style.color = "";
+        this.style.backgroundColor = "";
+    })
     //console.log(unitC);
     // escuchar por el evento
     convert[x].addEventListener("click",function(evento){
         evento.preventDefault();
         //coger la id de la etiqueta a
         laID =  evento.target.id;
-        console.log(laID);
+        //console.log(laID);
         let num = document.querySelector("input");
         // si num esta vacion
         //let message = (num.value === "") ? "no hay numero a convertir": "el numero a convertir es "+num.value;
@@ -35,29 +45,57 @@ for (x=0;x<convert.length;x++){
             h1.innerText = mensaje;
         }else if (laID == "KM"){
             let convertido = num.value * 0.6213711922;
-            let mensaje = (num.value +" es " + convertido+(" Kilometros"));
+            let mensaje = (num.value +" kilometros es " + convertido+" Millas");
+            h1.innerText = mensaje;
+        }else if (laID == "MK"){
+            let convertido = num.value * 1.60934;
+            let mensaje = (num.value +" Millas es " + convertido+" Kilometros");
             h1.innerText = mensaje;
         }else if (laID == "PK"){
             let convertido = num.value * 0.453592;
             let mensaje = (num.value +" es " + convertido+(" Kilogramos"));
             h1.innerText = mensaje;
-        }
-        else if(laID == "PL"){
-            let convertido = num.value / 1.1023; //factor de conversion obtenido dividiendo 500/453.592
+        }else if(laID == "PL"){
+            let convertido = num.value * 0.907194; // num.value/1.1023 factor de conversion obtenido dividiendo 500/453.592
             let mensaje = (num.value+" pounds es " + convertido+(" Libras (500 gr)"));
             h1.innerText = mensaje;
-        }else if(laID == "FC"){
-            let convertido = num.value - 32;
-            convertido = convertido/1.8; 
-            let mensaje = (num.value+" farenheit es " + convertido+ " celsius");
+        }
+        else if(laID == "FC"){
+            let convertido = (num.value -32)/1.8;
+            let mensaje = (num.value +" Farenheits es " + convertido+(" Celcius"));
             h1.innerText = mensaje;
-        }else if(laID == "PC"){
+        }
+        else if(laID == "PC"){
             let convertido = num.value * 2.54; //
             let mensaje = (num.value+" pulgadas es " + convertido+ " centimentros");
             h1.innerText = mensaje;
         }else if(laID == "CP"){
-            let convertido = num.value / 2.54;
+            let convertido = num.value * 0.393701;
             let mensaje = (num.value+" centimetros " + convertido+ " pulgadas");
+            h1.innerText = mensaje;
+        }else if(laID == "PM"){
+            let convertido = num.value * 0.0254;
+            let mensaje = (num.value+" pulgadas " + convertido+ " metros");
+            h1.innerText = mensaje;
+        }else if(laID == "MP"){
+            let convertido = num.value * 39.3701;
+            let mensaje = (num.value+" metros " + convertido+ " pulgadas");
+            h1.innerText = mensaje;
+        }else if(laID == "PIM"){
+            let convertido = num.value * .3048;
+            let mensaje = (num.value+" pies " + convertido+ " metros");
+            h1.innerText = mensaje;
+        }else if(laID == "MPI"){
+            let convertido = num.value * 3.28084;
+            let mensaje = (num.value+" metros " + convertido+ " pies");
+            h1.innerText = mensaje;
+        }else if(laID == "P2M2"){
+            let convertido = num.value * 0.092903;
+            let mensaje = (num.value+" pies cuadrados " + convertido+ " metros cuadrados");
+            h1.innerText = mensaje;
+        }else if(laID == "M2P2"){
+            let convertido = num.value * 10.7639;
+            let mensaje = (num.value+" metros cuadrados " + convertido+ " pies cuadrados");
             h1.innerText = mensaje;
         }
     })
